33bae907b7a95138cba49f917cf6d78bce51d88a
yujunghee <heeya230@gmail.com>
2015-10-07 13:58:47 +0900

    final code

---
+++ src/kr/ac/kookmin/embedded/api/ArrayListMagnet.java
@@ -0,0 +1,40 @@
+package kr.ac.kookmin.embedded.api;
+
+import java.util.ArrayList;
+
+public class ArrayListMagnet {
+	
+	public static void main(String[] arg){
+		ArrayList <String> a=new ArrayList <String>();
+		
+			a.add(0,"zero");
+			a.add(1,"one");
+			a.add(2,"two");
+			a.add(3,"three");
+			printAL(a);
+			
+			a.remove(2);
+			
+			if(a.contains("two")){
+				a.add("2.2");
+			}
+			
+			if(a.contains("three")){
+				a.add("four");
+			}
+			printAL(a);
+			
+			if(a.indexOf("four")!=4){
+				a.add(4,"4.2");
+			}
+			printAL(a);
+			printAL(a);
+			
+	}
+
+	public static void printAL(ArrayList<String>al){
+		for(String element:al){
+			System.out.print(element+" ");}
+		System.out.println(" ");
+	}
+}
